1d9d77accc6ddea2913ce4e9ecb8c546163198f7
ItaloCobains <monokatchaleger@gmail.com>
2022-10-18 15:58:24 -0300

    fix

--- main.go
+++ main.go
@@ -4,15 +4,10 @@ import (
 	"fmt"
 
 	"github.com/ItaloCobains/api-rest-go/database"
-	"github.com/ItaloCobains/api-rest-go/models"
 	"github.com/ItaloCobains/api-rest-go/routes"
 )
 
 func main() {
-	models.Personalidades = []models.Personalidade{
-		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
-		{Id: 2, Nome: "Nome 1", Historia: "Historia 1"},
-	}
 
 	database.ConectaComBancoDeDados()
 
